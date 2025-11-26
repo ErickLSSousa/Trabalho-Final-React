9ca85af875acd2d25121c4090eca068fb48bed25
pietro861 <pietrozagonel@gmail.com>
2025-11-26 11:23:46 -0300

    tentando arrumar o profile

--- Salles SousaStore/src/pages/Profile.jsx
+++ Salles SousaStore/src/pages/Profile.jsx
@@ -4,14 +4,13 @@ import { userAuth } from "../api/userAuth";
 export default function Profile() {
   const user = userAuth.getLogged();
 
-  return (
-    <main className="container">
-      <h1>Meu Perfil</h1>
-
-      <div className="admin-card" style={{ maxWidth: 350 }}>
-        <h3>Nome: {user.name}</h3>
-        <p>Email: {user.email}</p>
-      </div>
-    </main>
-  );
+  if (!user) {
+    return (
+      <main className="container">
+        <h1>Você não está logado</h1>
+        <h3>Nome: {user.name}</h3> 
+        <a href="/login">Fazer login</a>
+      </main>
+    );
+  }
 }
